30f64c424d606b84f9d35b0e9625c08a5c6fecc2
omersahin <1sahinomer1@gmail.com>
2021-05-20 20:38:41 +0300

    day 3

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -7,19 +7,7 @@ public class Main {
 		Product product1 = new Product(1,"lenovo v15",150000,"16gbram");
 
 		
-		Product product2 = new Product();
-		
-		product2.id=2;
-		product2.name="Lenovo V16";
-		product2.unitPrice=15000;
-		product2.detail="16gb ram";
-		
-		Product product3 = new Product();
-		
-		product3.id=3;
-		product3.name="Lenovo V17";
-		product3.unitPrice=15000;
-		product3.detail="16gb ram";
+	
 		
 		Product[] products = {
 				product1,product2,product3
--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -2,21 +2,46 @@ package oopIntro;
 
 public class Product {
 	
-	int id;
-	String name;
-	double unitPrice;
-	String detail;
+	private int id;
+	private String name;
+	private double unitPrice;
+	private String detail;
 	
-	public Product() {
-		System.out.println("Ben çalýþmýþam aðam");
-	}
+
 	public Product(int id,String name,double unitPrice,String detail) {
-		this(); // parametre almayan constructoru çaðýrýyor
+
 		this.id=id;
 		this.name=name;
 		this.unitPrice=unitPrice;
 		this.detail=detail;
 	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
 	
 
 	
