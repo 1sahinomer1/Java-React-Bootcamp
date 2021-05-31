591664275b784481c8010a0840f68fd78c789100
omersahin <1sahinomer1@gmail.com>
2021-05-31 12:50:00 +0300

    changes

--- Day4HW01/src/Main.java
+++ Day4HW01/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		BaseCustomerManager baseCustomerManager = new StarbuckCustomerManager(new MernisServiceAdapter());
-		baseCustomerManager.save(new Customer(1,"Ömer  faruk","Þahin",LocalDate.of(1998, 8, 30),"16141772010"));
+		baseCustomerManager.save(new Customer(1,"Ömer faruk","Þahin",LocalDate.of(1998, 8, 30),"16141772010"));
 		System.out.println();
 	}
 
